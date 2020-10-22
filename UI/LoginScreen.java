00aa378171aeb71da777a290916c9872da0aeb58
JeffsRazor <jeffthegrey8@gmail.com>
2020-10-21 21:14:22 -0400

    rebased

--- UI/LoginScreen.java
+++ UI/LoginScreen.java
@@ -1,6 +1,11 @@
+package UI;
+
 import java.awt.event.ActionListener;
+
 import java.awt.event.ActionEvent;
 
+
+import DatabaseCode.DatabaseCalls;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,18 +14,20 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class LoginScreen implements ActionListener {
-         private static JLabel userLabel;
-         private static JTextField usernameText;
-         private static JLabel passwordLabel;
-         private static JPasswordField passwordField;
-         private static JButton loginButton;
-         private static JLabel loginSucess;
-         private static JLabel title;
-
+    private static JLabel userLabel;
+    private static JTextField usernameText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordField;
+    private static JButton loginButton;
+    private static JLabel loginSucess;
+    private static JLabel title;
+    private static JFrame frame;
+    private static JPanel p;
+    
     public static void main(String[] args) {
         // Create frame and panel
-        JFrame frame = new JFrame();
-        JPanel p = new JPanel();
+         frame = new JFrame();
+         p = new JPanel();
         frame.setSize(600, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -55,24 +62,41 @@ public class LoginScreen implements ActionListener {
         loginSucess = new JLabel();
         loginSucess.setBounds(10, 130, 300, 25);
         p.add(loginSucess);
-        
 
         frame.setVisible(true);
 
     }
+    public void createLogin(){}
     @Override
     public void actionPerformed(ActionEvent e) {
         String user = usernameText.getText();
         String password = passwordField.getText();
+        int id = Integer.parseInt(user);
         System.out.println(user + " " + password);
         //make DB call here to check username/password (or maybe have login do ID instead of username)
-        if(user.equals("Jeff") && password.equals("poopybutts69")) {
+       try {
+           String dbPassword = DatabaseCalls.callDB(id);
+           System.out.println(dbPassword);
+           if(dbPassword.equals(password)) {
             loginSucess.setText("Login Successful!");
+            // if(DatabaseCalls.CallDB(id) >> function for finding admin bool in table)
+                //call hr dashboard
             //can instantiate the welcomescreen from here
             EmployeeDashboard.createDashboard();
-        }
-        else {
-            loginSucess.setText("Wrong credentials");
-        }
+            frame.dispose();
+            }
+            
+            else {
+                loginSucess.setText("Wrong credentials");
+            }
+        
+       } 
+       catch (Exception a) {
+        System.out.println("yeeet");
+       }
+       
     }
 }
+
+//querries 
+//SELECT employeeID from employee
