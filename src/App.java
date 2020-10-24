9e83fde85029f8103e117f9979f2518f6a44602f
JeffsRazor <jeffthegrey8@gmail.com>
2020-10-24 17:26:36 -0400

    now calls loginscreen to start program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.sql.Connection;
+
+import DatabaseCode.DatabaseConnection;
+import UI.LoginScreen;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Connection db = DatabaseConnection.getDatabase();
+        System.out.print(db);
+        //create login screen that will call the next dashboards
+        LoginScreen.createLogin(db);
+
     }
 }
