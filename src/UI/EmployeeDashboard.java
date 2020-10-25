143a3819d11c4568f9447b3b9b74f6c9142a080e
JeffsRazor <jeffthegrey8@gmail.com>
2020-10-25 19:42:24 -0400

    Changed the panel to use buttons instead of seperate panels

--- src/UI/EmployeeDashboard.java
+++ src/UI/EmployeeDashboard.java
@@ -19,15 +19,11 @@ import DatabaseCode.*;
 
 public class EmployeeDashboard {
     private static JFrame frame;
-    private static JLabel payrollInfo;
-    private static JLabel benefits;
+    
     private static JLabel title;
 
     private static JPanel panel1;
-    private static JPanel panel2;
-    private static JPanel panel3;
-    private static JPanel panel4;
-
+    
     private static JButton button;
 
     private static Employee emp;
@@ -38,39 +34,23 @@ public class EmployeeDashboard {
         frame.setSize(600, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
-        title = new JLabel(emp.getName());
+        title = new JLabel("Welcome, " + emp.getName());
         title.setBounds(0, 0, 300, 25);
+        
         panel1 = new JPanel();
-        panel2 = new JPanel();
-        panel3 = new JPanel();
-        panel4 = new JPanel();
 
-        button = new JButton("send");
+        button = new JButton("Logout");
+        
         panel1.setLayout(new FlowLayout());
         panel1.add(title);
         panel1.add(new JButton("Payroll Info"));
-        panel1.setBackground(Color.green);
+        panel1.add(new JButton("Profile Info"));
+        panel1.add(new JButton("Time Management"));
+        panel1.add(new JButton("Benefits"));
+        panel1.add(button);
+        panel1.setBackground(Color.white);
         panel1.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-
-        panel2.add(new JButton("Profile Info"));
-        panel2.setBackground(Color.green);
-        panel2.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-
-        panel3.add(new JButton("Time Management"));
-        panel3.setBackground(Color.green);
-        panel3.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-
-        //panel4.add(new JButton("Benefits"));
-        panel4.setBackground(Color.green);
-        panel4.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-        JLabel name = new JLabel(emp.getName());
-        name.setBounds(10, 70, 80, 25);
-        panel4.add(name);
        
-
-        panel1.add(panel2);
-        panel1.add(panel3);
-        panel1.add(panel4);
         frame.add(panel1);
         frame.setVisible(true);
     }
