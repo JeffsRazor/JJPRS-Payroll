2549b37a0267b873d3043efd8c1afdd5c7842235
JeffsRazor <jeffthegrey8@gmail.com>
2020-10-25 20:27:21 -0400

    Employee dashboard now has buttons that do stuff.

--- src/UI/EmployeeDashboard.java
+++ src/UI/EmployeeDashboard.java
@@ -3,8 +3,11 @@ package UI;
 import java.awt.*;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,25 +16,28 @@ import javax.swing.JPasswordField;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.plaf.basic.BasicButtonListener;
 
 import Employee.*;
 import DatabaseCode.*;
 
-public class EmployeeDashboard {
+public class EmployeeDashboard implements ActionListener{
     private static JFrame frame;
     
     private static JLabel title;
 
     private static JPanel panel1;
     
-    private static JButton button;
+    private static JButton logoutButton, payrollButton, profileButton, timeManagementButton, benefitsButton;
 
     private static Employee emp;
+
+    private static ImageIcon icon;
 
     public static void createDashboard(Connection db, int id) {
         emp = new Employee(db, id);
         frame = new JFrame();
-        frame.setSize(600, 1000);
+        frame.setSize(300, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         title = new JLabel("Welcome, " + emp.getName());
@@ -39,23 +45,81 @@ public class EmployeeDashboard {
         
         panel1 = new JPanel();
 
-        button = new JButton("Logout");
-        
+        logoutButton = new JButton("Logout");
+        logoutButton.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent evt)
+          {
+
+           LoginScreen.createLogin(db);
+           frame.dispose();
+          }
+        });
+        payrollButton = new JButton("Payroll Info");
+        payrollButton.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent evt)
+          {
+
+           //PayrollScreen.createDashboard(db);
+           frame.dispose();
+          }
+        });
+        profileButton = new JButton("Profile Info");
+        profileButton.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent evt)
+          {
+
+          // ProfileScreen.createProfile(db);
+           frame.dispose();
+          }
+        });
+
+        timeManagementButton = new JButton("Time Management");
+        timeManagementButton.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent evt)
+          {
+
+           //TimeScreen.createdashboard(db);
+           frame.dispose();
+          }
+        }); 
+
+        benefitsButton = new JButton("Benefits");
+        benefitsButton.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent evt)
+          {
+
+           //BenefitsScreen.createdashboard(db);
+           frame.dispose();
+          }
+        });
+
         panel1.setLayout(new FlowLayout());
         panel1.add(title);
-        panel1.add(new JButton("Payroll Info"));
-        panel1.add(new JButton("Profile Info"));
-        panel1.add(new JButton("Time Management"));
-        panel1.add(new JButton("Benefits"));
-        panel1.add(button);
+        panel1.add(payrollButton);
+        panel1.add(profileButton);
+        panel1.add(timeManagementButton);
+        panel1.add(benefitsButton);
+        panel1.add(logoutButton);
         panel1.setBackground(Color.white);
         panel1.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-       
+        
         frame.add(panel1);
+        
         frame.setVisible(true);
     }
-
+    
     public static void main(String args[]) throws SQLException {
         createDashboard(DatabaseConnection.getDatabase(), 12345);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
     }
 }
